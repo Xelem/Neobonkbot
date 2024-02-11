e4e538ec72993727071fda32ee5ee4c02d1f5fdb
Xelem <anselmchizurum@gmail.com>
2024-02-12 00:37:19 +0100

    Fixed another bug

--- controllers/lib/index.js
+++ controllers/lib/index.js
@@ -9,8 +9,9 @@ const messages = {
   home: "*Welcome to BonkBot* \n\nYou currently have a balance of *0.0022 SOL*, but no open positions.\n\nTo get started trading, you can open a position by buying a token.\n\nTo buy a token just enter a token address or paste a Birdeye link, and you will see a Buy dashboard pop up where you can choose how much you want to buy.\n\nAdvanced traders can enable Auto Buy in their settings. When enabled, BONKbot will instantly buy any token you enter with a fixed amount that you set. This is *disabled* by default.\n\nWallet:\n`2WCaM5j9uhpWjy2BiosMBq4XYQicXZK8ys84tSrKeJGk`",
   help: "*Help:*\n\n*Which tokens can I trade?*\nAny SPL token that is a Sol pair, on Raydium, Orca, and Jupiter. We pick up raydium pairs instantly, and Jupiter will pick up non sol pairs within around 15 minutes.\n\n*How can I see how much money I've made from referrals?*\nCheck the referrals button or type /referrals to see your payment in Bonk!\n\n*I want to create a new wallet on BONKbot.*\nClick the Wallet button or type /wallet, and you will be able to configure your new wallets\n\n*Is BONKbot free? How much do i pay for transactions?*\nBONKbot is completely free! We charge 1% on transactions, and keep the bot free so that anyone can use it.\n\n*Why is My Net Profit Lower Than Expected?*\nYour Net Profit is calculated after deducting all associated costs, including Price Impact, Transfer Tax, Dex Fees, and a 1% BONKbot fee. This ensures the figure you see is what you actually receive, accounting for all transaction-related expenses.\n\n*Is there a difference between @bonkbot_bot and the backup bots?*\nNo, they are all the same bot and you can use them interchangeably. If one is slow or down, you can use the other ones. You will have access to the same wallet and positions.\n\nFurther questions? Join our Telegram group: \nhttps://t.me/BONKbotChat  ",
   chat: "Join the disucssion, share bugs and feature requests in our telegram group: https://t.me/BONKbotChat",
+
   refer:
-    "*Referrals:*\n\nYour reflink: https://t.me/neobonk_bot?start\n\nReferrals: *0*\n\nLifetime Bonk earned: *0.00 BONK ($0.00)\n\nRewards are updated at least every 24 hours and rewards are automatically deposited to your BONK balance.\n\nRefer your friends and earn 30% of their fees in the first month, 20% in the second and 10% forever!",
+    "<b>Referrals:</b>\n\nYour reflink: https://t.me/neobonk_bot?start \n\nReferrals: <b>0</b>\n\nLifetime Bonk earned: <b>0.00 BONK</b> ($0.00)\n\nRewards are updated at least every 24 hours and rewards are automatically deposited to your BONK balance.\n\nRefer your friends and earn 30% of their fees in the first month, 20% in the second and 10% forever!",
 };
 
 const mainMenuKeyboard = {
@@ -140,12 +141,9 @@ bot.on("callback_query", (query) => {
       });
       break;
     case "refer":
-      console.log(query);
       bot.sendMessage(chatId, messages.refer, {
-        parse_mode: "Markdown",
-        reply_markup: {
-          inline_keyboard: [[{ text: "Close", callback_data: "close" }]],
-        },
+        parse_mode: "HTML",
+        reply_markup: menuKeyboard,
       });
       break;
     case "wallet":
